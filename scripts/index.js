00c7fab14bcae0f9edbdb5c4d761a7885bf304d4
e-zybkin <zybkin.evgeniy@gmail.com>
2021-10-11 15:11:12 +0300

    внести финальные правки

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,9 @@ const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-btn');
 const editButton = document.querySelector('.profile__edit-button');
 const form = document.querySelector('.popup__form');
-const nameField = document.querySelector('.popup__input_name');
+const nameField = document.querySelector('.popup__input_type_name');
 const nameOnPage = document.querySelector('.profile__name');
-const statusField = document.querySelector('.popup__input_status');
+const statusField = document.querySelector('.popup__input_type_status');
 const statusOnPage = document.querySelector('.profile__about');
 
 
